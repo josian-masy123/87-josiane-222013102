9c32fa2a5842c15b8afbb4bcd6a0461791f5e5f2
josian-masy123 <149627864+josian-masy123@users.noreply.github.com>
2023-11-03 12:36:53 +0200

    Add files via upload

---
+++ break.java
@@ -0,0 +1,11 @@
+class taad {
+    public static void main(String[] args){ 
+int i;
+        for(inti=0; i<=9; i++) {
+            if(i==4){
+                break;
+            }
+            System.out.println(i);
+        }
+    }
+}
