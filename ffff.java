de5a2af964a621637628f9f2d78ae49eef718fa1
josian-masy123 <149627864+josian-masy123@users.noreply.github.com>
2023-11-07 13:51:18 +0200

    Add files via upload

---
+++ ffff.java
@@ -0,0 +1,17 @@
+public class operator {
+ public static void main (String a[]);
+    {
+    int a=10;
+    int b=15;
+    if (b>a){
+     System.out.println("b is greater than a");
+    }
+    
+    else
+    {
+    system.out.println ("a is greater than b");
+        
+    }
+
+    }
+}
