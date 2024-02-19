5dc2ac13fb26e15da7ff85a7fbb15f6db160cca1
josian-masy123 <149627864+josian-masy123@users.noreply.github.com>
2024-02-19 13:42:42 +0200

    Add files via upload

---
+++ EXAM _MUSHIMIYIMANA josiane_222013102/database/login_form.sql
@@ -0,0 +1,51 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Feb 19, 2024 at 12:38 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `login form`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `userd`
+--
+
+CREATE TABLE `userd` (
+  `username` varchar(50) NOT NULL,
+  `phone` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `gender` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `userd`
+--
+
+INSERT INTO `userd` (`username`, `phone`, `email`, `gender`, `password`) VALUES
+('', '', '', '', ''),
+('joy', '', '', '', '123'),
+('', '', '', 'MALE', ''),
+('ttt', 'ff', 'tt', 'MALE', '45');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
